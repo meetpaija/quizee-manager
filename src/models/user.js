35e6574d370c5af5b2a3b89cc623a9f1e786e7b2
Meet Paija <meetp@amdocs.com>
2020-05-20 16:38:58 +0530

    Configure MongoDB, Added Users APIs

---
+++ src/models/user.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose'
+import validator from 'validator'
+
+const User = mongoose.model('User', {
+    username : {
+        type : String,
+        required : true,
+        trim : true
+    },
+    email : {
+        type : String,
+        required : true,
+        trim : true,
+        validate(value) {
+            if(!validator.isEmail(value)) {
+                throw new Error('Email is invalid')
+            }
+        }
+    },
+    password : {
+        type : String,
+        required : true,
+        trim : true,
+        minlength : 4
+    }
+})
+
+export default User;
